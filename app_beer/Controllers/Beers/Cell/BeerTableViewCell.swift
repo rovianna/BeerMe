b54b721b17ff57a21a51bc34df8ce83b2201a45d
Rodrigo Vianna <rodrigogvianna@outlook.com>
2018-12-27 10:09:37 -0200

    Image extension

---
+++ app_beer/Controllers/Beers/Cell/BeerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BeerTableViewCell.swift
+//  app_beer
+//
+//  Created by Rodrigo Vianna on 27/12/18.
+//  Copyright © 2018 Rodrigo Vianna. All rights reserved.
+//
+
+import UIKit
+
+class BeerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
