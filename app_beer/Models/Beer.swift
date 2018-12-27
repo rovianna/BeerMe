3a2d87ee17f803a58bd03973c23cfe7bf137a541
Rodrigo Vianna <rodrigogvianna@outlook.com>
2018-12-27 08:47:28 -0200

    First logic for the baseRequester and Beer Model

---
+++ app_beer/Models/Beer.swift
@@ -0,0 +1,9 @@
+//
+//  Beer.swift
+//  app_beer
+//
+//  Created by Rodrigo Vianna on 27/12/18.
+//  Copyright © 2018 Rodrigo Vianna. All rights reserved.
+//
+
+import Foundation
---
+++ app_beer/Requester/BaseRequester.swift
@@ -0,0 +1,9 @@
+//
+//  BaseRequester.swift
+//  app_beer
+//
+//  Created by Rodrigo Vianna on 27/12/18.
+//  Copyright © 2018 Rodrigo Vianna. All rights reserved.
+//
+
+import Foundation
