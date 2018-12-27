9649860509eed50e2ed4f0705e37597c4b01251f
Rodrigo Vianna <rodrigogvianna@outlook.com>
2018-12-27 11:59:57 -0200

    Detail for Beer

--- app_beer/Controllers/Beers/BeerDetailViewController.swift
+++ app_beer/Controllers/Beers/BeerDetailViewController.swift
@@ -15,11 +15,28 @@ class BeerDetailViewController: UIViewController {
         return vc
     }
     
+    @IBOutlet weak var beerImageView: UIImageView!
+    @IBOutlet weak var beerNameLabel: UILabel!
+    @IBOutlet weak var beerTaglineLabel: UILabel!
+    @IBOutlet weak var beerABVLabel: UILabel!
+    @IBOutlet weak var beerIBULabel: UILabel!
+    @IBOutlet weak var beerDescriptionLabel: UILabel!
+    
     var beer: Beer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         assert(beer != nil, "Must have a beer!")
+        configure(beer: beer)
+    }
+    
+    func configure(beer: Beer) {
+        beerImageView.downloadImage(from: beer.imageUrl)
+        beerNameLabel.text = beer.name
+        beerTaglineLabel.text = beer.tagline
+        beerABVLabel.text = "Teor alcoólico: \(beer.alcoholContent)"
+        beerIBULabel.text = "Escala de amargor: \(beer.bitternessScale)"
+        beerDescriptionLabel.text = beer.beerDescription
     }
     
     @IBAction func closeDetailAction(_ sender: UIButton) {
