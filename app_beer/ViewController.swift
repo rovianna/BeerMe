6e8380dc58b3761856486fac83e68399a15fca62
Rodrigo Vianna <rodrigogvianna@outlook.com>
2018-12-27 08:14:08 -0200

    Initial Commit

---
+++ app_beer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app_beer
+//
+//  Created by Rodrigo Vianna on 27/12/18.
+//  Copyright © 2018 Rodrigo Vianna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ app_beerTests/app_beerTests.swift
@@ -0,0 +1,34 @@
+//
+//  app_beerTests.swift
+//  app_beerTests
+//
+//  Created by Rodrigo Vianna on 27/12/18.
+//  Copyright © 2018 Rodrigo Vianna. All rights reserved.
+//
+
+import XCTest
+@testable import app_beer
+
+class app_beerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
